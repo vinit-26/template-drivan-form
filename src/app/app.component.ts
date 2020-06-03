e23a0694d4a9187e07b9c35ed1ebdab81f212a55
VINIT <vinitkumar7070@gmail.com>
2020-06-03 09:27:44 -0700

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'template-drivan-form';
+
+  submit(userDetails){
+    console.log(userDetails);
+  }
+}
